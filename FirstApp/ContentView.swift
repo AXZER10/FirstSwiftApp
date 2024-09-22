2a0d4a3d4c998701e7950c3f85b79532804c04c1
Pranjal Snehi <pranjal.snehi@gmail.com>
2024-09-23 03:41:35 +0530

    Further Learned about stacks and other things

--- FirstApp/ContentView.swift
+++ FirstApp/ContentView.swift
@@ -10,53 +10,47 @@ import SwiftUI
 struct ContentView: View {
     @Environment(\.colorScheme) var colorScheme
     var body: some View {
-        ZStack {
+        ZStack{
             Color("Color")
-                .ignoresSafeArea()
-            VStack {
-                HStack {
-                    Button(action: {
-                        // Action when button is tapped
-                        print("Button 1 tapped!")
-                    }) {
-                        Text("Button 1")
-                            .padding(7)
-                            .foregroundColor(.white) // Text color
-                            .background(Color.green) // Background color
-                            .cornerRadius(10) // Rounded corners
-                    }.padding(.horizontal)
-                    Button(action: {
-                        // Action when button is tapped
-                        print("Button 2 tapped!")
-                    }) {
-                        Text("Button 2")
-                            .padding(7)
-                            .foregroundColor(.black) // Text color
-                            .background(Color.cyan) // Background color
-                            .cornerRadius(10) // Rounded corners
-                    }.padding(.horizontal)
-                    Button(action: {
-                        // Action when button is tapped
-                        print("Button 3 tapped!")
-                    }) {
-                        Text("Button 3")
-                            .padding(7)
-                            .foregroundColor(.white) // Text color
-                            .background(Color.red) // Background color
-                            .cornerRadius(10) // Rounded corners
-                    }.padding(.horizontal)
-                }
-                Image("Asset1")
+            VStack(alignment: .leading, spacing: 20) {
+                Image("Image 1")
                     .resizable()
-                    .cornerRadius(20)
                     .aspectRatio(contentMode: .fit)
-                    .padding(.all)
-                Text("Hello! This is my first application")
-                    .font(.title2)
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.green/*@END_MENU_TOKEN@*/)
+                    .cornerRadius(20)
+                HStack{
+                    Text("Ooty Hill station")
+                        .fontWeight(.bold)
+                        .font(.title2)
+                    Spacer()
+                    VStack{
+                        HStack{
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.leadinghalf.fill")
+                            Image(systemName: "star")
+                        }
+                        .cornerRadius(3)
+                        .foregroundColor(.orange)
+                        .font(.caption)
+                        Text("1479 Reviews").font(.caption)
+                    }
+                }
+                Text("Come visit ooty for an experience of a lifetime.")
                     .fontWeight(.semibold)
-                    .foregroundColor(.white)
-            }
-        }
+                HStack{
+                    Spacer()
+                    Image(systemName: "fork.knife")
+                    Image(systemName: "binoculars.fill")
+                }.foregroundColor(.gray)
+                    .font(.caption)
+            }.padding(30)
+                .background(Color("Color 1"))
+                .cornerRadius(20)
+                .padding()
+                .shadow(radius: 30)
+        }.ignoresSafeArea()
             
     }
 }
