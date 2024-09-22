c3ac03fee846af7184b2076c0930d5446edfe583
Pranjal Snehi <pranjal.snehi@gmail.com>
2024-09-23 02:44:58 +0530

    Added Button with actions and backrounds changing based on system theme

--- FirstApp/ContentView.swift
+++ FirstApp/ContentView.swift
@@ -8,12 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.colorScheme) var colorScheme
     var body: some View {
         ZStack {
-            Color(.cyan)
+            Color("Color")
                 .ignoresSafeArea()
             VStack {
-               
+                HStack {
+                    Button(action: {
+                        // Action when button is tapped
+                        print("Button 1 tapped!")
+                    }) {
+                        Text("Button 1")
+                            .padding(7)
+                            .foregroundColor(.white) // Text color
+                            .background(Color.green) // Background color
+                            .cornerRadius(10) // Rounded corners
+                    }.padding(.horizontal)
+                    Button(action: {
+                        // Action when button is tapped
+                        print("Button 2 tapped!")
+                    }) {
+                        Text("Button 2")
+                            .padding(7)
+                            .foregroundColor(.black) // Text color
+                            .background(Color.cyan) // Background color
+                            .cornerRadius(10) // Rounded corners
+                    }.padding(.horizontal)
+                    Button(action: {
+                        // Action when button is tapped
+                        print("Button 3 tapped!")
+                    }) {
+                        Text("Button 3")
+                            .padding(7)
+                            .foregroundColor(.white) // Text color
+                            .background(Color.red) // Background color
+                            .cornerRadius(10) // Rounded corners
+                    }.padding(.horizontal)
+                }
                 Image("Asset1")
                     .resizable()
                     .cornerRadius(20)
