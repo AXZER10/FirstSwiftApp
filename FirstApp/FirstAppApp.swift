6acee26881f218df3dc33e979b22bba3e6fdff6b
Pranjal Snehi <pranjal.snehi@gmail.com>
2024-09-22 23:06:44 +0530

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Pranjal Snehi on 22/09/24.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FirstAppTests/FirstAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppTests.swift
+//  FirstAppTests
+//
+//  Created by Pranjal Snehi on 22/09/24.
+//
+
+import Testing
+@testable import FirstApp
+
+struct FirstAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
